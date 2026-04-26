8664f5e849ada7f35125b7b7d9cfb2750ea2ec59
jhegm722assignment <jhegarty281@gmail.com>
2026-04-26 10:10:20 +0100

    Addition of Corrine Landcover raster data. Check CLC crs.

--- egm722_assignment.py
+++ egm722_assignment.py
@@ -7,6 +7,7 @@ import numpy as np
 import rasterio as rio
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+from rasterio.plot import show
 
 # Define the full file path to the Lough Nillan SAC shapefile
 file_path_sac = r'C:\GIS_MSc\2026\EGM722_Programming_for_GIS&Remote_Sensing\Assignment\Spatial_Data\Lough_Nillan_SAC\lough_nillan_sac.shp'
@@ -25,3 +26,15 @@ sac_area = sac.minimum_rotated_rectangle
 
 # a sign of 1 means oriented counter-clockwise
 sac_area = shapely.geometry.polygon.orient(sac_area, sign=1)
+
+# Define the full file path to the Corrine Landcover raster data
+file_path_clc = r'C:\GIS_MSc\2026\EGM722_Programming_for_GIS&Remote_Sensing\Assignment\Spatial_Data\Satellite_Data\corine_land_cover_2018\64554\Results\u2018_clc2018_v2020_20u1_raster100m\u2018_clc2018_v2020_20u1_raster100m\DATA\U2018_CLC2018_V2020_20u1.tif'
+
+# Open the clc dataset
+with rasterio.open(file_path_clc) as src:
+    # Read the data (band 1) as a NumPy array
+    clc_data = src.read(1)
+
+    # Access metadata important for clc (CRS is usually EPSG:3035)
+    print(f"Coordinate Reference System: {src.crs}")
+    print(f"Data Type: {src.dtypes[0]}")
